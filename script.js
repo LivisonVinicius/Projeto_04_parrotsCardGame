7bd583cd40b25cdb7c49704dfd98a86a642902f9
LivisonVinicius <livisongc@gmail.com>
2022-04-06 18:32:57 -0300

    todos os requisitos

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
-let modoDeJogo = Number(prompt("Com quantas cartas quer jogar?",4))
 let contador = 0
+let counter = 0
+let contadorReset=0
 let numJogadas=0
-while(isNaN(modoDeJogo) || modoDeJogo%2!=0 || modoDeJogo<3|| modoDeJogo>14 ){
-    modoDeJogo = Number(prompt("Com quantas cartas quer jogar?",4))
+let arranjoClasse = [".bobrossparrot",".explodyparrot",".fiestaparrot",".metalparrot", ".revertitparrot",".tripletsparrot",".unicornparrot"]
+inicioDoJogo()
+function inicioDoJogo(){
+    let modoDeJogo = Number(prompt("Com quantas cartas quer jogar?",4))
+    while(isNaN(modoDeJogo) || modoDeJogo%2!=0 || modoDeJogo<3|| modoDeJogo>14 ){
+        modoDeJogo = Number(prompt("Com quantas cartas quer jogar?",4))
+    }
+    contador=0
+    counter=0
+    numJogadas=0
+    arranjoClasse = [".bobrossparrot",".explodyparrot",".fiestaparrot",".metalparrot", ".revertitparrot",".tripletsparrot",".unicornparrot"]
+    arranjoClasse.sort(parametro)
+    let arranjoGif=[`imagens/${arranjoClasse[0].replace(".","")}.gif`,`imagens/${arranjoClasse[0].replace(".","")}.gif`,`imagens/${arranjoClasse[1].replace(".","")}.gif`,`imagens/${arranjoClasse[1].replace(".","")}.gif`,`imagens/${arranjoClasse[2].replace(".","")}.gif`,`imagens/${arranjoClasse[2].replace(".","")}.gif`,`imagens/${arranjoClasse[3].replace(".","")}.gif`,`imagens/${arranjoClasse[3].replace(".","")}.gif`, `imagens/${arranjoClasse[4].replace(".","")}.gif`,`imagens/${arranjoClasse[4].replace(".","")}.gif`,`imagens/${arranjoClasse[5].replace(".","")}.gif`,`imagens/${arranjoClasse[5].replace(".","")}.gif`,`imagens/${arranjoClasse[6].replace(".","")}.gif`,`imagens/${arranjoClasse[6].replace(".","")}.gif`]
+    arranjoClasse=arranjoClasse.slice(7-modoDeJogo/2)
+    arranjoGif=arranjoGif.slice(14-modoDeJogo)
+    arranjoGif.sort(parametro)
+    while(contador<arranjoClasse.length){
+        const localizador1st= document.querySelector(arranjoClasse[contador])
+        const localizador2nd= document.querySelectorAll(arranjoClasse[contador])[1]
+        localizador1st.classList.remove("escondido")
+        localizador2nd.classList.remove("escondido")
+        localizador1st.querySelector(".back-face img").src=arranjoGif[counter+1]
+        localizador2nd.querySelector(".back-face img").src=arranjoGif[counter]
+        counter+=+2
+        contador=contador+1
+    }
 }
-let arranjo = [".bros",".explody",".fiesta",".metal", ".revertit",".triplets",".unicorn"]
-arranjo=arranjo.slice(7-modoDeJogo/2)
-console.log(arranjo)
-while(contador<arranjo.lenght){
-    const localizador1st= document.querySelector(arranjo[contador])
-    localizador1st.classList.remove("escondido")
-    contador=contador+1
-    console.log(arranjo)
+function parametro(){
+    return Math.random()-0.5;
 }
 
 function seleciona(elemento){
@@ -30,16 +49,19 @@ function verifica(){
     }
     document.querySelector(".verificando1").classList.remove("verificando1")
     document.querySelector(".verificando2").classList.remove("verificando2")
-    if (document.querySelectorAll(".virar").length==arranjo.length*2){
+    if (document.querySelectorAll(".virar").length==arranjoClasse.length*2){
         alert(`Você ganhou em ${numJogadas} jogadas!`)
-        let jogarDenovo=prompt("Quer jogar denovo?").toLowerCase()
-        if (jogarDenovo=="sim")
-        modoDeJogo = Number(prompt("Com quantas cartas quer jogar?",4))
+    //     let jogarDenovo=prompt("Quer jogar denovo?").toLowerCase()
+    //     if (jogarDenovo=="sim"){
+    //         while(document.querySelectorAll(".escondido").length<document.querySelectorAll(".card")){
+    //             let cartas=document.querySelector("main .card")
+    //             cartas.classList.add("escondido")
+    //             while(document.querySelectorAll(".virar").length!=0){
+    //                 document.querySelector(".virar").classList.remove("virar")
+    //             }
+    //             contadorReset++
+    //         }
+    //         inicioDoJogo()
+    //     }
     }
-    
 }
-
-
-
-
-
